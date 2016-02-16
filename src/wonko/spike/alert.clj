90cffe76ff9e423d384921edc1549c52d802e195
Srihari Sriraman <srihari@nilenso.com>
2016-02-16 21:30:09 +0530

    Conditionally send pager duty alert.
    - Alert only when [:options :alert] is set.
    - Extract alert-info specific details into a function.

--- src/wonko/spike/alert.clj
+++ src/wonko/spike/alert.clj
@@ -9,17 +9,23 @@
 ;; with pager duty.                                                    ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn pager-duty [{:keys [api-endpoint api-key] :as config}
-                  {:keys [metric-type metric-name properties] :as event}]
-  (try
-    (let [body {:service_key api-key
-                :event_type "trigger"
-                :description metric-name
-                :details event}]
-      (http/post api-endpoint
-                 {:content-type :json
-                  :body (json/encode body)
-                  :throw-exceptions true})
-      (spit "wonko.log" "Sent an alert to pagerduty!\n" :append true))
-    (catch Exception e
-      (spit "wonko.log" "Posting to pagerduty failed!\n" :append true))))
+(defn send-alert? [event]
+  (get-in event [:options :alert]))
+
+(defn alert-info [topic event]
+  {:description (:metric-name event)
+   :details (assoc event :topic topic)})
+
+(defn pager-duty [{:keys [api-endpoint api-key] :as config} topic event]
+  (when (send-alert? event)
+    (try
+      (let [body (merge {:service_key api-key
+                         :event_type "trigger"}
+                        (alert-info topic event))]
+        (http/post api-endpoint
+                   {:content-type :json
+                    :body (json/encode body)
+                    :throw-exceptions true})
+        (spit "wonko.log" "Sent an alert to pagerduty!\n" :append true))
+      (catch Exception e
+        (spit "wonko.log" "Posting to pagerduty failed!\n" :append true)))))
