a805169f284ba7c7bfa9b596c5773d731baa940f
Timothy Andrew <mail@timothyandrew.net>
2016-03-08 10:18:24 +0530

    Update the `validation` integration test to include all metric types.

--- test/wonko/core_test.clj
+++ test/wonko/core_test.clj
@@ -98,16 +98,26 @@
           {:keys [events-topic alerts-topic]} (tu/create-topics)]
 
       (tu/init-client service-name events-topic alerts-topic)
+
       (client/counter :validation-metric {:first 5})
       (is (thrown? IllegalArgumentException (client/counter :validation-metric {:first 6 :second 7})))
+
+      (client/gauge :validation-metric {:first 5} 5)
+      (is (thrown? IllegalArgumentException (client/gauge :validation-metric {:first 6 :second 7} 6)))
+
+      (client/stream :validation-metric {:first 5} 5)
+      (is (thrown? IllegalArgumentException (client/stream :validation-metric {:first 6 :second 7} 6)))
+
+      (client/alert :validation-metric {:first 5})
+      (is (client/alert :validation-metric {:first 6 :second 7}))
 
       (let [consumed-events (atom [])
             thread-pool (consume/start {events-topic 1 alerts-topic 1}
                                        (fn [event]
                                          (process event)
                                          (swap! consumed-events conj event)))]
-        (tu/wait-for #(= 1 (count @consumed-events)) :interval 1 :timeout 3)
-        (is (= (count @consumed-events) 1))
+        (tu/wait-for #(= 4 (count @consumed-events)) :interval 1 :timeout 3)
+        (is (= (count @consumed-events) 4))
 
         (consume/stop thread-pool)
         (tu/delete-topics events-topic alerts-topic)))))
